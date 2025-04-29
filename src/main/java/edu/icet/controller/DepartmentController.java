89f31d7f43d4ba94833c71096f9153f46aaf738f
Akash Fernando <Sathsarafernando11@gmail.com>
2025-04-29 11:15:59 +0530

    Create DepartmentController and DepartmentService

---
+++ src/main/java/edu/icet/controller/DepartmentController.java
@@ -0,0 +1,48 @@
+package edu.icet.controller;
+
+import edu.icet.dto.Department;
+import edu.icet.service.DepartmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@CrossOrigin
+@RequestMapping("/api/department")
+@RequiredArgsConstructor
+public class DepartmentController {
+    final DepartmentService service;
+
+    @PostMapping("/add")
+    public void addDepartment(@RequestBody Department department) {
+
+        service.addDepartment(department);
+    }
+
+    @GetMapping("/getAll")
+    public List<Department> getAll() {
+        return service.getAll();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteDepartment(@PathVariable("id") Integer departmentID) {
+        service.deleteDepartment(departmentID);
+    }
+
+    @PutMapping("/update-department")
+    public void updateStudent(@RequestBody Department student) {
+        service.updateDepartment(student);
+    }
+
+    @GetMapping("/search-by-id/{id}")
+    public Department searchById(@PathVariable("id") Integer departmentID) {
+
+        return service.searchById(departmentID);
+    }
+
+    @GetMapping("/search-by-name/{name}")
+    public List<Department> searchByName(@PathVariable("name") String departmentName) {
+
+        return service.searchByName(departmentName);
+    }
+}
---
+++ src/main/java/edu/icet/service/DepartmentService.java
@@ -0,0 +1,19 @@
+package edu.icet.service;
+
+import edu.icet.dto.Department;
+
+import java.util.List;
+
+public interface DepartmentService {
+    void addDepartment(Department department);
+
+    List<Department> getAll();
+
+    void deleteDepartment(Integer departmentID);
+
+    void updateDepartment(Department student);
+
+    Department searchById(Integer departmentID);
+
+    List<Department> searchByName(String departmentName);
+}
